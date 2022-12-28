b2178dc48f015747c685ff82560acea17d376086
Abdo Magdy <amagdy3@gmail.com>
2022-12-28 11:09:22 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,19 @@
-from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_file_handler, intent_handler
 from mycroft.util.parse import match_one
 from mycroft.audio import wait_while_speaking
-from hotels.py import *
+from mycroft.util.log import LOG
+from hotels.py import hotels.search()
 
 class HotelSearcher(MycroftSkill):
     def __init__(self):
-        self.register_entity_file('city.entity')
         MycroftSkill.__init__(self)
         self.is_reading = False
+        self.register_entity_file('city.entity')
+
         
     @intent_handler('searcher.hotel.intent')
-    def handle_searcher_hotel(self, message):
+     def handle_searcher_hotel(self, message):
         if message.data.get('city') is None:
             response = self.get_response('searcher.hotel')
             if response is None:
@@ -21,7 +24,7 @@ class HotelSearcher(MycroftSkill):
         hotels_result = hotels.search(response)
         self.log.info('Hotels result: {str(hotels_result)}')
         for hotel in hotels_result:
-            self.speak(hotel, wait=True)
+            self.speak_dialog(hotel, wait=True)
 
 def create_skill():
     return HotelSearcher()
