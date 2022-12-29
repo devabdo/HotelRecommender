31bb237a28837c00657605e2ba8c5329f39d525c
Abdo Magdy <amagdy3@gmail.com>
2022-12-29 15:51:18 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,19 @@
 from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill, intent_file_handler, intent_handler
+from mycroft import MycroftSkill, intent_handler
 from mycroft.util.parse import match_one
 from mycroft.audio import wait_while_speaking
 from mycroft.util.log import LOG
-from . import hotels
+from hotels import search
 
 class HotelSearcher(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        
+        """ The __init__ method is called when the Skill is first constructed.
+        It is often used to declare variables or perform setup actions, however
+        it cannot utilise MycroftSkill methods as the class does not yet exist. """
+        
+        super().__init__()
+        self.learning = True
         self.is_reading = False
         self.register_entity_file('city.entity')
 
@@ -19,11 +25,11 @@ class HotelSearcher(MycroftSkill):
                 return
         else:
             response = message.data.get('city')
-        self.speak_dialog('let_me_think', data={'city': response})
-        hotels_result = hotels.search(response)
-        self.log.info('Hotels result: {str(hotels_result)}')
-        for hotel in hotels_result:
-            self.speak(hotel, wait=True)
+            self.speak_dialog('let_me_think', data={'city': response})
+            hotels_result = hotels.search(response)
+            self.log.info('Hotels result: {str(hotels_result)}')
+            for hotel in hotels_result:
+                self.speak(hotel, wait=True)
 
 def create_skill():
     return HotelSearcher()
